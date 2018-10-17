31dcfbdbe20763c4ed7af7dff1db14027bc751ec
liuqi <timeless_lq@163.com>
2018-10-17 22:03:00 +0800

    [Update]:补充商品类目Service接口方法注释信息

--- src/main/java/com/immoc/sell/service/ProductCategoryService.java
+++ src/main/java/com/immoc/sell/service/ProductCategoryService.java
@@ -11,12 +11,35 @@ import java.util.List;
  */
 public interface ProductCategoryService {
 
+    /**
+     * 根据商品类目ID查询商品类目
+     *
+     * @param categoryId 商品类目ID
+     * @return 商品类目
+     */
     ProductCategory findById(Integer categoryId);
 
+    /**
+     * 查询所有商品类目集合
+     *
+     * @return 商品类目集合
+     */
     List<ProductCategory> findAll();
 
+    /**
+     * 根据指定类目编号列表查询对应商品类目集合
+     *
+     * @param categoryTypeList 指定类目编号列表
+     * @return 商品类目集合
+     */
     List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
 
+    /**
+     * 新增商品类目
+     *
+     * @param productCategory 商品类目
+     * @return 商品类目
+     */
     ProductCategory save(ProductCategory productCategory);
 
 }
